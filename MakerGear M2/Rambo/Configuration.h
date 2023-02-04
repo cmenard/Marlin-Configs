cec0db021bbe0a4128d44bdbda2b5493d4e10cc3
Curtis Menard <curtis.menard@gmail.com>
2023-02-04 10:13:50 -0500

    Correct Z steps Correct Z probe offset with Revo

--- MakerGear M2/Rambo/Configuration.h
+++ MakerGear M2/Rambo/Configuration.h
@@ -1170,7 +1170,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 88.88, 88.88, 408.94, 471.5 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 88.88, 88.88, 400, 471.5 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
@@ -1486,7 +1486,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -23.50, 0, -3.99 }
+#define NOZZLE_TO_PROBE_OFFSET { -23.50, 0, -4.00 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
